c8a49c71fcb41e2915b313b4fcf940cce05754f4
habibaatarek <150236504+habibaatarek@users.noreply.github.com>
2024-04-23 19:51:42 +0200

    commiting

---
+++ src/main/java/ejbs/App.java
@@ -0,0 +1,11 @@
+package ejbs;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+@ApplicationPath("/api")
+public class App extends Application {
+	
+		
+}
